118d99cd94df28cbb198407cd72c0324b396115b
Maxim <maxhard243@gmail.com>
2023-12-22 11:11:00 +0300

    style: add blur

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,15 @@ export default function Home() {
     setBgPosition(`${(window.innerWidth - evt.pageX) * 0.03}px ${(window.innerWidth - evt.pageY) * 0.03}px`);
   }
   return (
-    <main onMouseMove={nandleMousMove} style={{'backgroundPosition' : bgPosition}} className="bg-[#FFE6E6] flex h-screen items-end bg-pink-paws">
-      {/* <div className="border-l-emerald-50 overflow-hidden rotate-12">
-        <article className="w-[300px] h-[350px] rounded-3xl bg-fuchsia-400"></article>
-        <article className="w-[300px] h-[350px] rounded-3xl bg-fuchsia-400"></article>
-        <article className="w-[300px] h-[350px] rounded-3xl bg-fuchsia-400"></article>
-        <article className="w-[300px] h-[350px] rounded-3xl bg-fuchsia-400"></article>
-      </div> */}
+    <main onMouseMove={nandleMousMove} style={{'backgroundPosition' : bgPosition}} className="bg-[#FFE6E6] flex h-screen justify-between overflow-hidden bg-pink-paws">
+      <div className="border-l-emerald-50 overflow-hidden rotate-12 ml-28">
+        <article className="w-[300px] h-[350px] rounded-3xl bg-fuchsia-400 opacity-30 blur m-3"></article>
+        <article className="w-[300px] h-[350px] rounded-3xl bg-fuchsia-400 opacity-30 blur m-3"></article>
+        <article className="w-[300px] h-[350px] rounded-3xl bg-fuchsia-400 opacity-30 blur m-3"></article>
+        <article className="w-[300px] h-[350px] rounded-3xl bg-fuchsia-400 opacity-30 blur m-3"></article>
+      </div>
       <div>
-        <h1 className="animate-slide font-lilita font text-8xl">MY</h1>
+        <h1 className="animate-slide font-lilita font text-8xl mr-48">MY</h1>
       </div>
     </main>
   )
